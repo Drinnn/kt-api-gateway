b25a4ed687ba8c318dfd50b97673e9efa0730f8f
Drinnn <pedro_lontro@hotmail.com>
2021-01-18 16:22:43 -0300

    feat: add event emitter function

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,36 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Body,
+  Controller,
+  Logger,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import {
+  ClientProxy,
+  ClientProxyFactory,
+  Transport,
+} from '@nestjs/microservices';
+import { CreateCategoryDto } from './dtos/categories/categories.create.dto';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private readonly logger = new Logger(AppController.name);
+  private readonly adminClient: ClientProxy;
+  constructor() {
+    this.logger = new Logger(AppController.name);
+    this.adminClient = ClientProxyFactory.create({
+      transport: Transport.RMQ,
+      options: {
+        urls: [process.env.AMQP_CONNECTION_URL],
+        queue: 'admin',
+      },
+    });
+  }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('categories')
+  @UsePipes(ValidationPipe)
+  async createCategory(@Body() createCategoryDto: CreateCategoryDto) {
+    return await this.adminClient.emit('create-category', createCategoryDto);
   }
 }
